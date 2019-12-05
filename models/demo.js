77ab26d7d9b3bff72bda40225096129a93c0f0b0
GithubJackson <1359926897@qq.com>
2019-12-05 10:13:53 +0800

    fix: 规范注释

--- models/demo.js
+++ models/demo.js
@@ -1,27 +1,40 @@
 var { demoModel } = require('../schemas/index');
 
 /* 数据库操作函数 */
-// 查询
+
+/**
+ * @desc   查询
+ */
 async function getDemo(condition) {
   const res = await demoModel.find(condition)
   // const res = await demoModel.find(condition).populate('_otherModel') // 表关联查询
   return res
 }
-// 新建
+
+/**
+ * @desc   新建
+ */
 async function createDemo(query) {
   const res = await demoModel.create(query)
   return res
 }
-// 更新
+
+/**
+ * @desc   更新
+ */
 async function updateDemo(condition, query) {
   const res = await demoModel.update(condition, query)
   return res
 }
-// 删除
+
+/**
+ * @desc   删除
+ */
 async function delDemo(condition) {
   const res = await demoModel.remove(condition)
   return res
 }
+
 module.exports = {
   getDemo,
   createDemo,
